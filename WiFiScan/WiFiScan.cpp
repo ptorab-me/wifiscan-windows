bc975e91f191cdfab19fcd24b6547ad6e492c5c7
ptorab-parallels-win11 <ptorab@hotmail.com>
2023-07-14 10:36:17 -0700

    IE display bug, use pointer (PWLAN_BSS_ENTRY) to avoid structure copy

--- WiFiScan/WiFiScan.cpp
+++ WiFiScan/WiFiScan.cpp
@@ -52,39 +52,39 @@ int main() {
 
     if (dwResult == ERROR_SUCCESS) {
         for (DWORD i = 0; i < pBssList->dwNumberOfItems; i++) {
-            const WLAN_BSS_ENTRY bssEntry = pBssList->wlanBssEntries[i];
-            std::cout << "SSID: " << bssEntry.dot11Ssid.ucSSID << std::endl;
+            const PWLAN_BSS_ENTRY bssEntry = &pBssList->wlanBssEntries[i];
+            std::cout << "SSID: " << bssEntry->dot11Ssid.ucSSID << std::endl;
             std::cout << "BSSID: "; CHAR string[2 + 1];
-            for (DWORD j = 0; j < sizeof(bssEntry.dot11Bssid); j++) {
-                _snprintf_s(string, sizeof(string), "%02X", bssEntry.dot11Bssid[j]);
-                std::cout << string << (j < (sizeof(bssEntry.dot11Bssid) - 1) ? ":" : "\n");
+            for (DWORD j = 0; j < sizeof(bssEntry->dot11Bssid); j++) {
+                _snprintf_s(string, sizeof(string), "%02X", bssEntry->dot11Bssid[j]);
+                std::cout << string << (j < (sizeof(bssEntry->dot11Bssid) - 1) ? ":" : "\n");
             }
-            std::cout << "Scan radio identifier: " << bssEntry.uPhyId << std::endl;
-            std::cout << "RSSI: " << bssEntry.lRssi << " dBm" << std::endl;
+            std::cout << "Scan radio identifier: " << bssEntry->uPhyId << std::endl;
+            std::cout << "RSSI: " << bssEntry->lRssi << " dBm" << std::endl;
             //std::cout << "Link quality: " << bssEntry.uLinkQuality << std::endl;
-            std::cout << "PHY type: " << bssEntry.dot11BssPhyType << std::endl;
+            std::cout << "PHY type: " << bssEntry->dot11BssPhyType << std::endl;
             std::cout << "Operating in designated regulatory domain: " <<
-                (bssEntry.bInRegDomain ? "True" : "False") << std::endl;
-            std::cout << "Beacon interval: " << bssEntry.usBeaconPeriod * 1.024 << " msec" << std::endl;
-            //std::cout << "Beacon or Probe Response timestamp: " << bssEntry.ullTimestamp << std::endl;
-            //std::cout << "Host timestamp: " << bssEntry.ullHostTimestamp << std::endl;
-            std::cout << "Capability information bitmap: " << bssEntry.usCapabilityInformation << std::endl;
-            std::cout << "Channel: " << bssEntry.ulChCenterFrequency / 1000.0 << " MHz" << std::endl;
+                (bssEntry->bInRegDomain ? "True" : "False") << std::endl;
+            std::cout << "Beacon interval: " << bssEntry->usBeaconPeriod * 1.024 << " msec" << std::endl;
+            //std::cout << "Beacon or Probe Response timestamp: " << bssEntry->ullTimestamp << std::endl;
+            //std::cout << "Host timestamp: " << bssEntry->ullHostTimestamp << std::endl;
+            std::cout << "Capability information bitmap: " << bssEntry->usCapabilityInformation << std::endl;
+            std::cout << "Channel: " << bssEntry->ulChCenterFrequency / 1000.0 << " MHz" << std::endl;
             std::cout << "Rate set in Mbps: ";
-            DWORD numberOfRates = bssEntry.wlanRateSet.uRateSetLength / sizeof(USHORT);
+            DWORD numberOfRates = bssEntry->wlanRateSet.uRateSetLength / sizeof(USHORT);
             for (DWORD j = 0; j < numberOfRates; j++) {
-                std::cout << (bssEntry.wlanRateSet.usRateSet[j] & 0x7FFF) * 0.5 <<
+                std::cout << (bssEntry->wlanRateSet.usRateSet[j] & 0x7FFF) * 0.5 <<
                     ((j < numberOfRates - 1) ? ", " : "\n");
             }
-            std::cout << "IE buffer offset: " << bssEntry.ulIeOffset << std::endl;
-            std::cout << "IE buffer size: " << bssEntry.ulIeSize << std::endl;
+            std::cout << "IE buffer offset: " << bssEntry->ulIeOffset << std::endl;
+            std::cout << "IE buffer size: " << bssEntry->ulIeSize << std::endl;
 
-            // IE retrieval fails; IE blob is absent based on memory dumps around &bssEntry
-            // const PBYTE ieBuffer = (const PBYTE)((const PBYTE)(&bssEntry) + bssEntry.ulIeOffset);
-            // for (DWORD j = 0; j < bssEntry.ulIeSize; j++) {
-            //    printf("%02X ", ieBuffer[j]);
-            // }
-            // std::cout << std::endl;
+            // display IEs
+            const PBYTE ieBuffer = (PBYTE)bssEntry + bssEntry->ulIeOffset;
+            for (DWORD j = 0; j < bssEntry->ulIeSize; j++) {
+               printf("%02X ", ieBuffer[j]);
+            }
+            std::cout << std::endl;
        
             std::cout << std::endl;
 
